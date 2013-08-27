0d372f34824050351aac1a4b2ae585ef3c63098e
Eric Driggers <admalledd@gmail.com>
2013-08-27 10:56:17 -0700

    seems i missed regenerating the files as part of the publishing. oops.
    also updated makefile to reflect that I have to use a more custom release method.

--- publish.sh
+++ publish.sh
@@ -1,15 +1,23 @@
 
+echo "publishing files to the site..."
+
+
 
 TARGET=mc.admalledd.com:/var/www/
 #TARGET=/home/admalledd/Documents/notes/www
 SRC=/home/admalledd/Documents/notes/pelican_test/output
 
+echo "first, clean and regenerate the output files"
+make clean
+make html
+
+echo "now setting up symlinks"
 HOMEWWW=/home/admalledd/www
 ln -s $HOMEWWW/dl/ $SRC/dl
 ln -s $HOMEWWW/mc/ $SRC/mc
 ln -s $HOMEWWW/favicon.ico $SRC/
 cp $SRC/../htaccess $SRC/.htaccess
-
+echo "transferring files..."
 rsync -avz --del $SRC/ $TARGET/
 
 #ln -s $TARGET/pages/mc-index.html $TARGET/mc/index.html
